a0fd95ec96cf1b4087cd47e5ee6811cc144df97c
Alexey Golyshin <stas2z@gmail.com>
2021-02-12 23:23:33 +0300

    heavy update, mostly modified allocation and deallocation for dynamic use, add some callbacks, added arduino independed pins init through LL etc

--- cPins.cpp
+++ cPins.cpp
@@ -1,86 +1,96 @@
-#include <Arduino.h>
+#include <wiring.h>
+
+#include "stm32_def.h"
+#include "stm32yyxx_ll_gpio.h"
+
+#include <vector>
+
+#ifndef CPIN_DISABLE_HWTIMER
 #include <HardwareTimer.h>
+#endif
 
 #include "cPins.h"
 
-uint16_t cPins::pinCounter = 0;
 uint64_t cPins::timerCounter = 0;
-uint32_t cPins::timerFreq = 0;
-uint32_t cPins::localCounter = 0;
+uint32_t cPins::timerFreq = 1;
 uint32_t cPins::prevms = 0;
 uint8_t cPins::ledBrightness = 100;
+#ifndef CPIN_DISABLE_HWTIMER
 HardwareTimer *cPins::T = nullptr;
 bool cPins::timerInited = false;
-cPins **cPins::pinsList;
-cPins **cPins::localList;
-
-void cPins::pushInstance(cPins *instance)
-{
-  pinsList = (cPins **)realloc((void *)pinsList, (pinCounter + 1) * sizeof(instance));
-  localList = (cPins **)realloc((void *)localList, (pinCounter + 1) * sizeof(instance));
-  pinsList[pinCounter++] = instance;
-}
+#endif
 
 cPins::cPins(const char *cname, uint16_t _PIN, uint8_t isled, uint8_t hs)
-    : port(get_GPIO_Port(STM_PORT(digitalPin[_PIN]))),
-      pin(STM_PIN(digitalPin[_PIN])), pinHW(_PIN), isHW(isled & 2U),
-      highState(hs & 0x1), lowState(!(hs & 1U)), isLed(isled & 1U)
-{
+    : port(get_GPIO_Port(STM_PORT(digitalPinToPinName(_PIN)))),
+      pin(STM_GPIO_PIN(digitalPinToPinName(_PIN))), pinHW(_PIN),
+      isHW(isled & 2U), highState(hs & 1U), lowState(!(hs & 1U)),
+      isLed(isled & 1U) {
   uint8_t isOD = hs & 2U;
-  pinMode(_PIN, isOD ? OUTPUT_OPEN_DRAIN : OUTPUT);
+  uint32_t ll_pin = STM_LL_GPIO_PIN(digitalPinToPinName(pinHW));
+  set_GPIO_Port_Clock(STM_PORT(digitalPinToPinName(pinHW)));
+  LL_GPIO_SetPinMode(port, ll_pin, LL_GPIO_MODE_OUTPUT);
+  LL_GPIO_SetPinOutputType(
+      port, ll_pin, isOD ? LL_GPIO_OUTPUT_OPENDRAIN : LL_GPIO_OUTPUT_PUSHPULL);
+#ifndef STM32F1xx
+  LL_GPIO_SetPinPull(port, ll_pin, LL_GPIO_PULL_NO);
+#endif
+  LL_GPIO_SetPinSpeed(port, ll_pin, LL_GPIO_SPEED_FREQ_HIGH);
+#ifndef CPIN_DISABLE_HWTIMER
   if ((isHW) && !(pin_in_pinmap(digitalPinToPinName(_PIN), PinMap_PWM)))
+#endif
   {
     isHW = 0;
   }
   reset();
   name = (char *)malloc(strlen(cname) + 1);
   strcpy(name, cname);
-  if (pinCounter < CPINS_MAX)
-  {
-    pushInstance(this);
-  }
+  instances.push_back(this);
+  _me = instances.end();
   setPWM(PWMMAX);
 }
-inline void cPins::write(uint8_t value)
-{
-  port->BSRR = (1U << pin) << (!(value & 1U) << 4);
+inline void cPins::write(uint8_t value) {
+  port->BSRR = (pin) << (!(value & 1U) << 4);
 }
-inline uint8_t cPins::read(void) { return port->IDR & (1U << pin); }
+// only to read output ports, input ports should be read from IDR
+inline uint8_t cPins::read(void) { return port->ODR & (pin); }
 inline void cPins::reset(void) { write(lowState); }
 inline void cPins::set(void) { write(highState); }
-inline void cPins::toggle(void) { port->ODR ^= (1U << pin); }
+inline void cPins::toggle(void) { port->ODR ^= (pin); }
 bool cPins::isActive(void) { return (blinkTime == 0); }
-void cPins::setTime(uint32_t time)
-{
+uint32_t cPins::getRemaining(void) { return blinkTime; }
+uint32_t cPins::getTime(void) {
+  // if (isActive())
+  return blinkInitTime;
+  // else
+  //  return 0;
+}
+void cPins::setTime(uint32_t time) {
+#ifndef CPIN_DISABLE_HWTIMER
   bool timerPaused = false;
 
-  if (timerInited)
-  { // threadsafe, don't bother blinkTime till we changing
+  if (timerInited) { // threadsafe, don't bother blinkTime till we changing
     // it here
     T->pause();
     timerPaused = true;
   }
-  if (!blinkTime)
-  { // if pin finished any job already, just init it as NORMAL
+#endif
+  if (!blinkTime) { // if pin finished any job already, just init it as NORMAL
     // and reset
     blinkInitTime = blinkTime = time;
     phaseShift = 0;
-  }
-  else
-  {
-    switch (mode)
-    {
+  } else {
+    switch (mode) {
     default:
     case CPIN_NORMAL:
       blinkTime = blinkInitTime = time;
       phaseShift = 0;
       break;
-    case CPIN_ADDITIVE:                 // simply add some extra time to it
-      if (blinkInitTime + time <= ~0UL) // check for overflow
+    case CPIN_ADDITIVE:             // simply add some extra time to it
+      if (blinkTime + time <= ~0UL) // check for overflow
         blinkTime += time;
       else
         blinkTime = ~0UL;
-      if (blinkTime + time <= ~0UL) // same check here
+      if (blinkInitTime + time <= ~0UL) // same check here
         blinkInitTime += time;
       else
         blinkInitTime = ~0UL;
@@ -95,27 +105,25 @@ void cPins::setTime(uint32_t time)
       break;
     }
   }
+#ifndef CPIN_DISABLE_HWTIMER
   if (timerPaused) // switch timer on back
     T->resume();
+#endif
 }
 void cPins::noInterrupt(void) { noInterruptable = true; }
-void cPins::setMode(uint8_t newMode)
-{
+void cPins::setMode(uint8_t newMode) {
   if (newMode < CPIN_MODEEND)
     mode = newMode;
 }
 
-void cPins::setPWM(uint16_t duty)
-{
-  if (duty <= PWMMAX)
-  {
+void cPins::setPWM(uint16_t duty) {
+  if (duty <= PWMMAX) {
     Duty = duty;
     pwmDuty = isLed ? pwmLED[((ledBrightness * (Duty << 8)) / 100) >> 8]
                     : Duty; // magic duty calcs for led :)
   }
 }
-void cPins::blink(uint32_t time, uint32_t period)
-{
+void cPins::blink(uint32_t time, uint32_t period) {
   if (noInterruptable && blinkTime)
     return;
   else
@@ -124,8 +132,7 @@ void cPins::blink(uint32_t time, uint32_t period)
   blinkPeriod = period;
   toBreathe = 0;
 }
-void cPins::breathe(uint32_t time, uint32_t period)
-{
+void cPins::breathe(uint32_t time, uint32_t period) {
   if (noInterruptable && blinkTime)
     return;
   else
@@ -135,8 +142,7 @@ void cPins::breathe(uint32_t time, uint32_t period)
   toBreathe = 1;
   blinkFade = 0;
 }
-void cPins::blinkfade(uint32_t time, uint32_t period)
-{
+void cPins::blinkfade(uint32_t time, uint32_t period) {
   if (noInterruptable && blinkTime)
     return;
   else
@@ -146,8 +152,7 @@ void cPins::blinkfade(uint32_t time, uint32_t period)
   toBreathe = 1;
   blinkFade = 1;
 }
-void cPins::on(uint32_t time)
-{
+void cPins::on(uint32_t time) {
   if (noInterruptable && blinkTime)
     return;
   else
@@ -156,169 +161,148 @@ void cPins::on(uint32_t time)
   blinkPeriod = 0;
   toBreathe = 0;
 }
-void cPins::off()
-{
+void cPins::off() {
   if (noInterruptable && blinkTime)
     return;
   else
     noInterruptable = false;
-  blinkTime = blinkInitTime = 0;
+  finishCallback(this); // call callback at the end
+  blinkTime = 0;
   blinkPeriod = 0;
   toBreathe = 0;
   blinkFade = 0;
-}
-void cPins::on()
-{
-  if (noInterruptable && blinkTime)
-    return;
-  else
-    noInterruptable = false;
-  blinkTime = ~0UL;
-  blinkPeriod = 0;
-  toBreathe = 0;
-  blinkFade = 0;
+  blinkInitTime = 0;
 }
 uint16_t cPins::getDuty(void) { return (Duty); }
 
-void cPins::setBrightness(uint8_t brightness)
-{
+void cPins::setBrightness(uint8_t brightness) {
   if (brightness > 100)
     ledBrightness = 100;
   else
     ledBrightness = brightness;
 
-  for (uint16_t cpin = 0; cpin < pinCounter; cpin++)
-  {
-    if (pinsList[cpin]->isLed == CPIN_LED)
-      pinsList[cpin]->setPWM(pinsList[cpin]->getDuty());
+  for (uint16_t cpin = 0; cpin < instances.size(); cpin++) {
+    if (instances[cpin]->isLed == CPIN_LED)
+      instances[cpin]->setPWM(instances[cpin]->getDuty());
   }
 }
-void cPins::timerCallback(HardwareTimer *ht)
-{
+
+void cPins::timerCallback(void) {
   uint32_t counter = timerCounter % ticksPerCycle;
   uint32_t ms = (((timerCounter * 1000)) / (timerFreq * ticksPerCycle));
-  if (ms > prevms)
-  {
-    localCounter = 0;
-    for (uint32_t c = 0; c < pinCounter; c++)
-    {
-      if (pinsList[c]->blinkTime == 0)
-      {
-        if ((pinsList[c]->isHW) && (pinsList[c]->prevDuty > 0))
-        {
-          pwm_stop(digitalPinToPinName(pinsList[c]->pinHW));
-          pinsList[c]->prevDuty = 0;
+  if (ms > prevms) {
+    std::vector<cPins *> oldinstances = localinstances;
+    localinstances.clear();
+    for (uint32_t c = 0; c < instances.size(); c++) {
+      if (instances[c]->blinkTime == 0) {
+        if ((instances[c]->isHW) && (instances[c]->prevDuty > 0)) {
+#ifndef CPIN_DISABLE_HWTIMER
+          pwm_start(digitalPinToPinName(instances[c]->pinHW),
+                    instances[c]->pinFrequency, 0, (TimerCompareFormat_t)8);
+#endif
+          instances[c]->prevDuty = 0;
         }
-        pinsList[c]->reset();
-      }
-      else
-      {
-        if (pinsList[c]->blinkTime > ms - prevms) {
-          pinsList[c]->blinkTime-= ms - prevms;
+        for (uint32_t old = 0; old < oldinstances.size(); old++) {
+          if (instances[c] == oldinstances[old]) {
+            instances[c]->finishCallback(
+                instances[c]); // call callback at the end
+            instances[c]->reset();
+            break;
+          }
+        }
+        if (instances[c]->blinkInitTime > 0) {
+          instances[c]->blinkInitTime = 0;
+        }
+      } else {
+        if (instances[c]->blinkTime > ms - prevms) {
+          instances[c]->blinkTime -= ms - prevms;
         } else {
-          pinsList[c]->blinkTime = 0;
+          instances[c]->blinkTime = 0;
         }
-        localList[localCounter++] = pinsList[c];     
+        localinstances.push_back(instances[c]);
       }
+      instances[c]->tickCallback(instances[c]); // call every tick callback
     }
     prevms = ms;
-        
   }
-  for (uint16_t c = 0; c < localCounter; c++)
-  {
-    if (!counter)
-    {
-      if (localList[c]->blinkPeriod)
-      {
+  for (uint16_t c = 0; c < localinstances.size(); c++) {
+    if (!counter) {
+      if (localinstances[c]->blinkPeriod) {
         uint32_t period =
-            ((localList[c]->blinkInitTime - localList[c]->blinkTime + 1 +
-              localList[c]->phaseShift) /
-             (localList[c]->blinkPeriod >> 1)) &
+            ((localinstances[c]->blinkInitTime - localinstances[c]->blinkTime +
+              1 + localinstances[c]->phaseShift) /
+             (localinstances[c]->blinkPeriod >> 1)) &
             1UL;
-        if (localList[c]->toBreathe)
-        {
+        if (localinstances[c]->toBreathe) {
           uint32_t _tduty;
-          if (localList[c]->isLed)
-          {
-            _tduty = pwmLED[((ledBrightness * (localList[c]->getDuty() << 8)) / 100) >> 8];
+          if (localinstances[c]->isLed) {
+            _tduty =
+                pwmLED[((ledBrightness * (localinstances[c]->getDuty() << 8)) /
+                        100) >>
+                       8];
+          } else {
+            _tduty = localinstances[c]->pwmDuty;
           }
-          else
-          {
-            _tduty = localList[c]->pwmDuty;
+          uint32_t step = ((localinstances[c]->blinkInitTime -
+                            localinstances[c]->blinkTime + 1 +
+                            localinstances[c]->phaseShift) %
+                           (localinstances[c]->blinkPeriod >> 1));
+          if ((!period) && (localinstances[c]->blinkFade)) {
+            localinstances[c]->tempDuty = _tduty;
+          } else {
+            uint32_t tempDuty =
+                (((_tduty << 8) / (localinstances[c]->blinkPeriod >> 1)) *
+                 step) >>
+                8;
+            localinstances[c]->tempDuty = period ? _tduty - tempDuty : tempDuty;
           }
-          uint32_t step =
-              ((localList[c]->blinkInitTime - localList[c]->blinkTime + 1 +
-                localList[c]->phaseShift) %
-               (localList[c]->blinkPeriod >> 1));
-          if ((!period) && (localList[c]->blinkFade))
-          {
-            localList[c]->tempDuty = _tduty;
-          }
-          else
-          {
-            uint32_t tempDuty = (((_tduty << 8) / (localList[c]->blinkPeriod >> 1)) * step) >> 8;
-            localList[c]->tempDuty = period ? _tduty - tempDuty : tempDuty;
-          }
+        } else {
+          localinstances[c]->tempDuty = period ? localinstances[c]->pwmDuty : 0;
         }
-        else
-        {
-          localList[c]->tempDuty = period ? localList[c]->pwmDuty : 0;
-        }
+      } else {
+        localinstances[c]->tempDuty = localinstances[c]->pwmDuty;
       }
-      else
-      {
-        localList[c]->tempDuty = localList[c]->pwmDuty;
-      }
-      if (localList[c]->isHW)
-      {
-        if (localList[c]->prevDuty != localList[c]->tempDuty)
-        {
-          localList[c]->prevDuty = localList[c]->tempDuty;
-          pwm_start(digitalPinToPinName(localList[c]->pinHW), CPIN_HWFREQ,
-                    localList[c]->lowState == 0 ? localList[c]->tempDuty
-                                                : 255 - localList[c]->tempDuty,
+#ifndef CPIN_DISABLE_HWTIMER
+      if (localinstances[c]->isHW) {
+        if (localinstances[c]->prevDuty != localinstances[c]->tempDuty) {
+          localinstances[c]->prevDuty = localinstances[c]->tempDuty;
+          pwm_start(digitalPinToPinName(localinstances[c]->pinHW),
+                    localinstances[c]->pinFrequency,
+                    localinstances[c]->lowState == 0
+                        ? localinstances[c]->tempDuty
+                        : 255 - localinstances[c]->tempDuty,
                     (TimerCompareFormat_t)8);
         }
-      }
-      else
+      } else
+#endif
       {
-        if (localList[c]->tempDuty)
-        {
-          localList[c]->set();
-        }
-        else
-        {
-          localList[c]->reset();
+        if (localinstances[c]->tempDuty) {
+          localinstances[c]->set();
+        } else {
+          localinstances[c]->reset();
         }
       }
-    }
-    else
-    {
-      if ((!localList[c]->isHW) && (localList[c]->blinkTime) &&
-          ((uint32_t)(localList[c]->tempDuty + 1) == counter))
-      {
-        localList[c]->reset();
+    } else {
+      if ((!localinstances[c]->isHW) && (localinstances[c]->blinkTime) &&
+          ((uint32_t)(localinstances[c]->tempDuty + 1) == counter)) {
+        localinstances[c]->reset();
       }
     }
   }
   timerCounter++;
 }
-void cPins::initTimer(TIM_TypeDef *timer, uint32_t freq)
-{
+#ifndef CPIN_DISABLE_HWTIMER
+void cPins::initTimer(TIM_TypeDef *timer, uint32_t freq) {
   timerFreq = freq;
-  if (timerInited)
-  {
+  if (timerInited) {
     freeTimer();
   }
 
-  for (uint16_t c = 0; c < pinCounter; c++)
-  {
-    if (pinsList[c]->isHW == 1)
-    {
-      PinName p = digitalPinToPinName(pinsList[c]->pinHW);
-      if (timer == (TIM_TypeDef *)pinmap_peripheral(p, PinMap_PWM))
-      {
-        pinsList[c]->isHW = 0;
+  for (uint16_t c = 0; c < instances.size(); c++) {
+    if (instances[c]->isHW == 1) {
+      PinName p = digitalPinToPinName(instances[c]->pinHW);
+      if (timer == (TIM_TypeDef *)pinmap_peripheral(p, PinMap_PWM)) {
+        instances[c]->isHW = 0;
       }
     }
   }
@@ -330,25 +314,28 @@ void cPins::initTimer(TIM_TypeDef *timer, uint32_t freq)
   timerInited = true;
   T->resume();
 }
-void cPins::freeTimer(void)
-{
+void cPins::freeTimer(void) {
   T->pause();
   delete T;
   timerInited = false;
   timerFreq = 0;
 }
-cPins::~cPins()
-{
-  pinCounter--;
-  if (pinCounter)
-  {
-    pinsList = (cPins **)realloc((void *)pinsList, pinCounter * sizeof(cPins *));
-    localList = (cPins **)realloc((void *)localList, pinCounter * sizeof(cPins *));
-  }
-  if (!pinCounter && timerInited)
-    freeTimer(); // normally it should never happen cuz all pins have to be
-                 // statically defined, but if you want to malloc cpins
-                 // instatnces, it will free timer after last pin deletion
+#endif
+cPins::~cPins() {
   if (name != nullptr)
     free(name);
+  instances.erase(_me);
+#ifndef CPIN_DISABLE_HWTIMER
+  if (instances.empty() && timerInited)
+    freeTimer(); // normally it should never happen cuz all pins have to be
+// statically defined, but if you want to malloc cpins
+// instances, it will free timer after last pin deletion
+#endif
 }
+#ifndef CPIN_DISABLE_HWTIMER
+void cPins::setFrequency(uint32_t frequency) {
+  if (isHW) {
+    pinFrequency = frequency;
+  }
+}
+#endif
--- cPins.h
+++ cPins.h
@@ -1,12 +1,8 @@
 #ifndef CPINS_H
 #define CPINS_H
 
-#include <Arduino.h>
 #include <HardwareTimer.h>
-
-#ifndef CPINS_MAX
-#define CPINS_MAX 100
-#endif
+#include <sketch.h>
 
 #define PWMMAX 256
 
@@ -14,26 +10,9 @@
 #define CPIN_HWFREQ 1000
 #endif
 
-enum cpinledtype
-{
-  CPIN_PIN = 0,
-  CPIN_LED = 1,
-  CPIN_HWPIN = 2,
-  CPIN_HWLED = 3
-};
-enum cpinmode
-{
-  CPIN_NORMAL = 0,
-  CPIN_ADDITIVE,
-  CPIN_CONTINUE,
-  CPIN_MODEEND
-};
-enum cpinstate
-{
-  CPIN_LOW = 0,
-  CPIN_HIGH = 1,
-  CPIN_OD = 2
-};
+enum cpinledtype { CPIN_PIN = 0, CPIN_LED = 1, CPIN_HWPIN = 2, CPIN_HWLED = 3 };
+enum cpinmode { CPIN_NORMAL = 0, CPIN_ADDITIVE, CPIN_CONTINUE, CPIN_MODEEND };
+enum cpinstate { CPIN_LOW = 0, CPIN_HIGH = 1, CPIN_OD = 2 };
 
 static const uint8_t pwmLED[257] = {
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01,
@@ -59,16 +38,19 @@ static const uint8_t pwmLED[257] = {
     0xFD, 0xFD, 0xFD, 0xFD, 0xFD, 0xFD, 0xFD, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE,
     0xFE, 0xFE, 0xFF, 0xFF, 0xFF};
 
-class cPins
-{
+class cPins {
 private:
-  GPIO_TypeDef *port;              // lowlevel GPIO port
-  uint16_t pin;                    // lowlevel GPIO pin
-  uint32_t pinHW;                  // arduino pin num
-  uint8_t isHW = 0;                // is hardware pwm?
-  uint8_t highState, lowState;     // pin states gpio level
-  uint16_t pwmDuty = 0;            // pwm duty cycle value after led correction
-  uint16_t Duty = 0;               // pwm duty cycle before led correction (=pwmDuty if not led)
+  GPIO_TypeDef *port; // lowlevel GPIO port
+  uint16_t pin;       // lowlevel GPIO pin
+  uint32_t pinHW;     // arduino pin num
+#ifndef CPIN_DISABLE_HWTIMER
+  uint32_t pinFrequency = CPIN_HWFREQ; // default frequency for HW pin
+#endif
+  uint8_t isHW = 0;            // is hardware pwm?
+  uint8_t highState, lowState; // pin states gpio level
+  uint16_t pwmDuty = 0;        // pwm duty cycle value after led correction
+  uint16_t Duty =
+      0; // pwm duty cycle before led correction (=pwmDuty if not led)
   uint32_t blinkPeriod = 0;        // period of blink/breath etc
   uint32_t toBreathe = 0;          // is configured as breathe
   volatile uint32_t blinkTime = 0; // time of blink/breath etc
@@ -78,28 +60,30 @@ private:
   uint8_t isLed = 0;               // is configured as led
   uint32_t phaseShift = 0;         // phase shift value
   bool noInterruptable = false;    // thread safe mutex
-  static uint32_t localCounter;    // count of active pins container
-  static cPins **localList;        // local container only for active pins
-
-  static HardwareTimer *T;                   // hardware timer object
-  static bool timerInited;                   // is timer inited or not
-  static uint32_t timerFreq, prevms;         // timer frequency and previos millisecond value
-  static void pushInstance(cPins *instance); // push new pin to pinsList
-  static uint8_t ledBrightness;              // global brightness for leds
-  void setTime(uint32_t time);               // set pin time value depending on current mode
+  static uint32_t prevms;          // previous millisecond value
+  static uint8_t ledBrightness;    // global brightness for leds
+  void setTime(uint32_t time); // set pin time value depending on current mode
 
 public:
+#ifndef CPIN_DISABLE_HWTIMER
+  static HardwareTimer *T; // hardware timer object
+  static bool timerInited; // is timer inited or not
+#endif
   static uint64_t timerCounter;                 // pin instances counter
+  static uint32_t timerFreq;                    // timer frequency
   static const uint32_t ticksPerCycle = PWMMAX; // timer ticks per duty cycle
-  static uint16_t pinCounter;                   // static counter for inited pins, public only for
-                                                // testing purposes only
-  static cPins **pinsList;                      // list of inited pins
-  char *name = (char *)nullptr;                 // pin name, filled by CPIN define, can be used
-                                                // for debug purposes
-  uint16_t tempDuty = 0;                        // current calculated duty for pin
-  uint16_t prevDuty = 0xFFFF;                   // previous calculated duty
+#undef __inst__
+#define __inst__ std::vector<cPins *>
+  inline static __inst__ localinstances = {};
+  inline static __inst__ instances = {};
+  __inst__::iterator _me;
+  char *name = (char *)NULL;  // pin name, filled by CPIN define, can be used
+                              // for debug purposes
+  uint16_t tempDuty = 0;      // current calculated duty for pin
+  uint16_t prevDuty = 0xFFFF; // previous calculated duty
 
-  cPins(const char *cname, uint16_t _PIN, uint8_t isled = CPIN_PIN, uint8_t hs = CPIN_HIGH);
+  cPins(const char *cname, uint16_t _PIN, uint8_t isled = CPIN_PIN,
+        uint8_t hs = CPIN_HIGH);
   // CPIN_PIN will init pin as usual gpio, use CPIN_LED to make brightness
   // change better for leds hs = 0 is for active low pins
 
@@ -110,42 +94,65 @@ public:
   __always_inline void toggle(void);
   __always_inline uint8_t read(void);
 
-  bool isActive(void); // return false when its off
+  bool isActive(void);         // return false when its off
+  uint32_t getRemaining(void); // return 0 when its off and ms value when on
+  uint32_t getTime(void);      // return working time given at start
 
   void setPWM(uint16_t duty); // set PWM duty for pin
 
   uint16_t getDuty(void); // for internal use
 
-  void blink(uint32_t time, uint32_t period = 0); // blink, 1/2 of period it will be on, 1/2 of period
-                                                  // - off, if period is 0 its equal to .on(time)
+  void blink(
+      uint32_t time,
+      uint32_t period = 0); // blink, 1/2 of period it will be on, 1/2 of period
+                            // - off, if period is 0 its equal to .on(time)
 
-  void breathe(uint32_t time, uint32_t period = 0); // breathing, 1/2 period softly increasing
-                                                    // brightness and decreasing after
+  void breathe(uint32_t time,
+               uint32_t period = 0); // breathing, 1/2 period softly increasing
+                                     // brightness and decreasing after
 
-  void blinkfade(uint32_t time, uint32_t period = 0); // 1/2 period will be ON, next 1/2 will fadeout
+  void blinkfade(
+      uint32_t time,
+      uint32_t period = 0); // 1/2 period will be ON, next 1/2 will fadeout
 
-  void on(uint32_t time); // just switch it on for a specific time
+  void on(uint32_t time = ~0UL); // just switch it on for a specific time
 
   void off(); // switch off
 
-  void on(); // switch on for infinite time
-
-  void setMode(uint8_t newMode); // normally any new call will reset previous state
-                                 // CPIN_CONTINUE allows to set new time without changing
-                                 // period CPIN_ADDITIVE will add time with each new call
+  void setMode(
+      uint8_t newMode); // normally any new call will reset previous state
+                        // CPIN_CONTINUE allows to set new time without changing
+                        // period CPIN_ADDITIVE will add time with each new call
 
   void noInterrupt(void); // make pin noninterruptable, any new calls will be
                           // ignored till previously set job will expire
 
-  static void setBrightness(uint8_t brightness); // set global brightness (for CPIN_LED pins only)
+  static void setBrightness(
+      uint8_t brightness); // set global brightness (for CPIN_LED pins only)
 
-  static void timerCallback(HardwareTimer *ht); // callback for timer routines
+  void setPhase(uint32_t phase) {
+    phaseShift = phase;
+  } // manually set phase shift
+#ifndef CPIN_DISABLE_HWTIMER
+  void setFrequency(uint32_t frequency);
+#endif
 
-  static void initTimer(TIM_TypeDef *timer = TIM1, uint32_t freq = 60); // by default it will use TIM1 at 60Hz, dont
-                                                                        // forget to init it at startup
+#ifndef CPIN_DISABLE_HWTIMER
+  static void timerCallback(void); // callback for timer routines
+
+  static void
+  initTimer(TIM_TypeDef *timer = TIM1,
+            uint32_t freq = 60); // by default it will use TIM1 at 60Hz, dont
+                                 // forget to init it at startup
 
   static void freeTimer(void);
-
+#else
+  static void timerCallback(void); // callback for timer routines
+#endif
+  std::function<void(cPins *pin)> finishCallback = [](cPins *) {
+  }; // callback called after time expiring or off command
+  std::function<void(cPins *pin)> tickCallback = [](cPins *) {
+  }; // callback called every ms
   ~cPins();
 };
 
